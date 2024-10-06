2a3d21f4f99fa9db4e53d83df8f8b42fd81e4194
Tran Thanh Dat <neban0444@gmail.com>
2024-10-06 22:26:51 +0700

    add student/lecturer id, school feild in user

--- model/user.js
+++ model/user.js
@@ -6,6 +6,14 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
+    userCode: {
+        type: String,
+        required: true,
+    },
+    school:{
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
